be2a4d2276f857244c1f5131b01eca7f33865daf
Cap Bao Dien <diencb@innotech-vn.com>
2023-08-28 16:49:06 +0700

    fix: bind handler event for secondary action button

--- components/modals/modal.tsx
+++ components/modals/modal.tsx
@@ -60,23 +60,23 @@ export const Modal = ({
 
   return (
     <>
-      <div className="flex items-center justify-center overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none bg-neutral-800/70">
-        <div className="relative w-full md:w-4/6 lg:w-3/6 xl:w-2/5 my-6 mx-auto h-full lg:h-auto md:h-auto">
+      <div className="fixed inset-0 z-50 flex items-center justify-center overflow-y-auto overflow-x-hidden bg-neutral-800/70 outline-none focus:outline-none">
+        <div className="relative mx-auto my-6 h-full w-full md:h-auto md:w-4/6 lg:h-auto lg:w-3/6 xl:w-2/5">
           {/* CONTENT */}
           <div
             className={`
               translate
-              duration-300
               h-full
+              duration-300
               ${showModal ? 'translate-y-0' : 'translate-y-full'}
               ${showModal ? 'opacity-100' : 'opacity-0'}
             `}
           >
-            <div className="translate h-full lg:h-auto md:h-auto border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
+            <div className="translate relative flex h-full w-full flex-col rounded-lg border-0 bg-white shadow-lg outline-none focus:outline-none md:h-auto lg:h-auto">
               {/* HEADER */}
-              <div className="flex items-center justify-center p-6 rounded-t relative border-b-[1px]">
+              <div className="relative flex items-center justify-center rounded-t border-b-[1px] p-6">
                 <button
-                  className="p-1 border-0 hover:opacity-70 transition absolute left-9"
+                  className="absolute left-9 border-0 p-1 transition hover:opacity-70"
                   onClick={handleClose}
                 >
                   <IoMdClose size={18} />
@@ -86,17 +86,17 @@ export const Modal = ({
               </div>
 
               {/* BODY */}
-              <div className="relative p-6 flex-auto">{body}</div>
+              <div className="relative flex-auto p-6">{body}</div>
 
               {/* FOOTER */}
               <div className="flex flex-col gap-2 p-6">
-                <div className="flex flex-row items-center gap-4 w-full">
+                <div className="flex w-full flex-row items-center gap-4">
                   {secondaryAction && secondaryActionLabel && (
                     <Button
                       outline
                       label={secondaryActionLabel}
                       disabled={disabled}
-                      onClick={secondaryAction}
+                      onClick={handleSecondaryAction}
                     />
                   )}
                   <Button label={actionLabel} disabled={disabled} onClick={handleSubmit} />
